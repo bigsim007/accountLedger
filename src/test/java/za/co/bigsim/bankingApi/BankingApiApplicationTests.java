175692b0f852785ad869b5ae2ea9a9a7c4c516e7
smusabaike <simbarashe.musabaike@altron.com>
2023-05-13 09:56:53 +0200

    first commit

---
+++ src/test/java/za/co/bigsim/bankingApi/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bigsim.bankingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
